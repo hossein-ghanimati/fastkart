e404cf08afcb35407328d9e9bee78f1da442c7a1
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-05-22 19:48:33 +0330

    Debug Pagination

--- assets/js/index/funcs/shared.js
+++ assets/js/index/funcs/shared.js
@@ -24,12 +24,7 @@ const renderProductsSlider = () => {
     loop: true,
     autoplay: {
       delay: 2000,
-    },
-    pagination: {
-      el : '.swiper-pagination',
-      dynamicBullets : true,
-      clickable : true,
-    },
+    }
   })
 }
 
