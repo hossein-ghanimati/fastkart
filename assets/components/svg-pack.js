4476df03c016f8fc759bf5aeef802fd7c64b769d
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-06-03 19:14:19 +0330

    Coding newsletter Section

--- assets/components/svg-pack.js
+++ assets/components/svg-pack.js
@@ -293,7 +293,13 @@ svgPackTemplate.innerHTML = `
             d="m19 12h-14m0 0 7 7m-7-7 7-7" stroke="currentColor" /><path
             d="m19 12h-14m0 0 7 7m-7-7 7-7" stroke="url(#b)"
             stroke-opacity="currentOpacity" /></g>
-      </symbol>
+    </symbol>
+
+    <symbol id="envelop-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+        stroke-width="1.5" stroke="currentColor" class="size-6">
+        <path stroke-linecap="round" stroke-linejoin="round"
+            d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+    </symbol>
 
 
 </svg>
