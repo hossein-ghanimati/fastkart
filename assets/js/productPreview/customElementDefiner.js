fb50350efcb9322029acd9aa81e66915728216e6
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-06-21 14:38:25 +0330

    Create 'product-other-details' Component

--- assets/js/productPreview/customElementDefiner.js
+++ assets/js/productPreview/customElementDefiner.js
@@ -1,5 +1,7 @@
 import CommentBox from "../../components/productPreview/boxes/comment-box.js";
 import ProductMainDetails from "../../components/productPreview/product-main-details.js";
+import ProductOtherDetails from "../../components/productPreview/product-other-details.js";
 
 customElements.define('product-main-details', ProductMainDetails)
 customElements.define('comment-box', CommentBox)
+customElements.define('product-other-details', ProductOtherDetails)
