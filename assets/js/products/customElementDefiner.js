239694616dec333af82fbddd85649d7245454a1a
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-06-24 13:44:49 +0330

    Create 'FilteringOptions' Component

--- assets/js/products/customElementDefiner.js
+++ assets/js/products/customElementDefiner.js
@@ -1,5 +1,7 @@
 import productRowBox from "../../components/products/boxes/product-row-box.js";
 import FilteringNav from "../../components/products/filtering-nav.js";
+import FilteringOptions from "../../components/products/filtering-options.js";
 
 customElements.define('product-row-box', productRowBox)
 customElements.define('filtering-nav', FilteringNav)
+customElements.define('filtering-options', FilteringOptions)
